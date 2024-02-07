0fb4c43d34b1a2d72651ff9e62784c25582dc1cc
limseonghyun <sksrpf1126@naver.com>
2024-02-07 15:18:53 +0900

    [level 0] Title: 배열 조각하기, Time: 0.09 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr, int[] query) {
+        int[] answer = {};
+        
+        answer = Arrays.copyOfRange(arr, 0, query[0] + 1);
+        
+        for(int i = 1; i < query.length; i++) {
+            if(i % 2 == 0) {
+                answer = Arrays.copyOfRange(answer, 0, query[i] + 1);
+            }else {
+                answer = Arrays.copyOfRange(answer, query[i], answer.length);
+            }
+        }
+        
+        return answer;
+    }
+}
