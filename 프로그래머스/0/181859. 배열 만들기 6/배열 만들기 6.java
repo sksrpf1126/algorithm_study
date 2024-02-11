b94529a7511f44e5303b403bff49ae34d9355daf
limseonghyun <sksrpf1126@naver.com>
2024-02-11 15:42:55 +0900

    [level 0] Title: 배열 만들기 6, Time: 25.16 ms, Memory: 83.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        
+        List<Integer> list = new ArrayList<>();
+        
+        for(int i = 0; i < arr.length; i++) {
+            if(list.size() == 0) list.add(arr[i]);
+            else if(list.get(list.size() - 1) == arr[i]) list.remove(list.size() - 1);
+            else if(list.get(list.size() - 1) != arr[i]) list.add(arr[i]);
+        }
+        
+        if(list.size() == 0) list.add(-1);
+        
+        return list.stream().mapToInt((v) -> v).toArray();
+    }
+}
