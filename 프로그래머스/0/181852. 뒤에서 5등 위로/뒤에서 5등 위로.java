11238f4aa54aab33e8c57bb777ebd24ee21f09ad
limseonghyun <sksrpf1126@naver.com>
2024-02-12 14:49:33 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 0.50 ms, Memory: 67.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer;
+        
+        Arrays.sort(num_list);
+        
+        answer = Arrays.copyOfRange(num_list, 5, num_list.length);
+        
+        return answer;
+    }
+}
