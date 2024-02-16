3752e55bf9f7470d50ffd08c7a277c1f88ad5bd1
limseonghyun <sksrpf1126@naver.com>
2024-02-16 16:08:03 +0900

    [level 0] Title: 배열 조각하기, Time: 0.07 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.java
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.java
@@ -2,6 +2,26 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] arr, int[] query) {
+//         int[] answer = {};
+//         int[] temp = {};
+//         ArrayList<Integer> list = new ArrayList<>();
+        
+//         for(int i=0; i<query.length; i++){
+//             if(query[i]%2==0 && i ==0){
+//                temp = Arrays.copyOfRange(arr,0,query[i]+1);
+              
+//             }else if(query[i]%2==1 && i ==1){
+//                 temp = Arrays.copyOfRange(arr,query[i],arr.length);                
+//             }else if(query[i]%2==0&& i !=0){
+//                 temp = Arrays.copyOfRange(temp,0,query[i]+1);
+                
+//             }else if(query[i]%2==1 && i !=1){
+//                 temp = Arrays.copyOfRange(temp,query[i],arr.length);
+            
+//             }
+//         }
+//         return temp;
+
         int[] answer = {};
         
         answer = Arrays.copyOfRange(arr, 0, query[0] + 1);
