e98339325a99bf4bd91496a64cea6e3045ada39d
limseonghyun <sksrpf1126@naver.com>
2024-02-16 10:12:23 +0900

    [level 0] Title: 리스트 자르기, Time: 0.04 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
@@ -2,22 +2,48 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int n, int[] slicer, int[] num_list) {
-        int[] answer = {};
-        
-        switch(n) {
-            case 1: answer = Arrays.copyOfRange(num_list, 0, slicer[1] + 1); break;
-            case 2: answer = Arrays.copyOfRange(num_list, slicer[0], num_list.length); break;
-            case 3: answer = Arrays.copyOfRange(num_list, slicer[0], slicer[1] + 1); break;
-            case 4: 
-                List<Integer> list = new ArrayList<>();
+                int[] answer = {};
+        int a =  slicer[0];
+        int b =  slicer[1];
+        int c =  slicer[2];
+        ArrayList<Integer> list = new ArrayList<>();
+        switch(n){
+            case 1 : answer = Arrays.copyOfRange(num_list,0,b+1);
+                     break;
+                    
+            case 2 : answer = Arrays.copyOfRange(num_list,a,num_list.length);
+                     break;
                 
-                for(int i = slicer[0]; i <= slicer[1]; i += slicer[2]) {
-                    list.add(num_list[i]);
-                } 
+            case 3 :answer = Arrays.copyOfRange(num_list,a,b+1);
+                     break;
                 
-                answer = list.stream().mapToInt((v) -> v).toArray();
+            case 4 : for(int i = a; i < b + 1 ; i += c){
+                      list.add(num_list[i]);
+                          }
+                //새로 정의하지 않고 그냥 new int[]만 해서 배열 크기 선언 가능
+                answer = new int[list.size()];
+                for(int i=0;i<list.size();i++){
+                    answer[i]=list.get(i);
+                }
+                break;
         }
-        
         return answer;
+//         int[] answer = {};
+        
+//         switch(n) {
+//             case 1: answer = Arrays.copyOfRange(num_list, 0, slicer[1] + 1); break;
+//             case 2: answer = Arrays.copyOfRange(num_list, slicer[0], num_list.length); break;
+//             case 3: answer = Arrays.copyOfRange(num_list, slicer[0], slicer[1] + 1); break;
+//             case 4: 
+//                 List<Integer> list = new ArrayList<>();
+                
+//                 for(int i = slicer[0]; i <= slicer[1]; i += slicer[2]) {
+//                     list.add(num_list[i]);
+//                 } 
+                
+//                 answer = list.stream().mapToInt((v) -> v).toArray();
+//         }
+        
+//         return answer;
     }
 }
