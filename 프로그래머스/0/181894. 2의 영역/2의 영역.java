8f163276a93109ae3ad2d86df3231736c078b437
limseonghyun <sksrpf1126@naver.com>
2024-02-16 12:13:24 +0900

    [level 0] Title: 2의 영역, Time: 2.51 ms, Memory: 120 MB -BaekjoonHub

--- 프로그래머스/0/181894. 2의 영역/2의 영역.java
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.java
@@ -3,14 +3,30 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] arr) {
         int[] answer = {};
+        ArrayList<Integer> list = new ArrayList<>();
         
-        int first = firstIdxSearch(arr, 2);
-        int end = endIdxSearch(arr, 2);
-        
-        if(first == -1 || end == -1) answer = new int[]{-1};
-        else answer = Arrays.copyOfRange(arr, first, end + 1);
-        
+        //2가 들어가는 idx 추출해서 list에 넣기
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i] == 2){
+                list.add(i);
+            } 
+            //list 첫번째, 끝 idx로 배열값 복사 
+        }
+        if(list.size() == 0) answer = new int[]{-1};
+        else {
+            answer = Arrays.copyOfRange(arr,list.get(0),list.get(list.size() - 1) + 1);
+        }
+    
         return answer;
+//         int[] answer = {};
+        
+//         int first = firstIdxSearch(arr, 2);
+//         int end = endIdxSearch(arr, 2);
+        
+//         if(first == -1 || end == -1) answer = new int[]{-1};
+//         else answer = Arrays.copyOfRange(arr, first, end + 1);
+        
+//         return answer;
     }
     
     public static int firstIdxSearch(int[] arr, int value) {
