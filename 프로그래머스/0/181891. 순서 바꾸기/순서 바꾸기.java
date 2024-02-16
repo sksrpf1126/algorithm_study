501b13532e8d82c23279ad85eabf890eef341fdc
limseonghyun <sksrpf1126@naver.com>
2024-02-16 19:21:30 +0900

    [level 0] Title: 순서 바꾸기, Time: 0.02 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
@@ -1,17 +1,35 @@
 class Solution {
     public int[] solution(int[] num_list, int n) {
-        int[] answer = new int[num_list.length];
-        
-        int idx = 0;
-        
-        for(int i = n; i < num_list.length; i++) {
-            answer[idx++] = num_list[i];
+ int[] answer = new int[num_list.length];
+        int t=0;  
+        int temp = n;
+            for(int i = 0; i< num_list.length-n; i++){
+                answer[i] = num_list[temp++];
+            }
+        if(n != 1){
+         for(int i = num_list.length-n; i< num_list.length; i++){
+                answer[i] = num_list[t++];
+            }
+        }else if(n==1){
+            for(int i = num_list.length-n; i< num_list.length; i++){
+                answer[i] = num_list[t++];
+            }
         }
-        
-        for(int i = 0; i < n; i++) {
-            answer[idx++] = num_list[i];
-        }
-        
         return answer;
+
+        
+        //         int[] answer = new int[num_list.length];
+        
+//         int idx = 0;
+        
+//         for(int i = n; i < num_list.length; i++) {
+//             answer[idx++] = num_list[i];
+//         }
+        
+//         for(int i = 0; i < n; i++) {
+//             answer[idx++] = num_list[i];
+//         }
+        
+//         return answer;
     }
 }
