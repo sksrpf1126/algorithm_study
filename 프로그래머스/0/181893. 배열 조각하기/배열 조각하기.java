51527e2dcaa0a963f0d6edc8e4c8b28ad14d714d
limseonghyun <sksrpf1126@naver.com>
2024-02-16 16:14:03 +0900

    [level 0] Title: 배열 조각하기, Time: 0.09 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.java
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.java
@@ -2,31 +2,12 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] arr, int[] query) {
-//         int[] answer = {};
-//         int[] temp = {};
-//         ArrayList<Integer> list = new ArrayList<>();
         
-//         for(int i=0; i<query.length; i++){
-//             if(query[i]%2==0 && i ==0){
-//                temp = Arrays.copyOfRange(arr,0,query[i]+1);
-              
-//             }else if(query[i]%2==1 && i ==1){
-//                 temp = Arrays.copyOfRange(arr,query[i],arr.length);                
-//             }else if(query[i]%2==0&& i !=0){
-//                 temp = Arrays.copyOfRange(temp,0,query[i]+1);
-                
-//             }else if(query[i]%2==1 && i !=1){
-//                 temp = Arrays.copyOfRange(temp,query[i],arr.length);
-            
-//             }
-//         }
-//         return temp;
-
-        int[] answer = {};
+        int[] answer = arr;
         
-        answer = Arrays.copyOfRange(arr, 0, query[0] + 1);
+        // answer = Arrays.copyOfRange(arr, 0, query[0] + 1);
         
-        for(int i = 1; i < query.length; i++) {
+        for(int i = 0; i < query.length; i++) {
             if(i % 2 == 0) {
                 answer = Arrays.copyOfRange(answer, 0, query[i] + 1);
             }else {
