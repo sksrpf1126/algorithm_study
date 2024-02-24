9e2898ac112055821dfad1b0ae3ad9492104d490
limseonghyun <sksrpf1126@naver.com>
2024-02-24 19:39:47 +0900

    [level 0] Title: 각도기, Time: 0.02 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int angle) {
+        int answer = -1;
+        if(angle == 180) {
+            answer = 4;
+        }else if(90 < angle && angle < 180) {
+            answer = 3;
+        }else if(angle == 90) {
+            answer = 2;
+        }else {
+            answer = 1;
+        }
+            
+            
+        return answer;
+    }
+}
