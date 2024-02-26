111bc51d8675af89dc724175353c9d9c7e1d5152
limseonghyun <sksrpf1126@naver.com>
2024-02-26 16:03:35 +0900

    [level 0] Title: 그림 확대, Time: 11.00 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/0/181836. 그림 확대/그림 확대.java
+++ 프로그래머스/0/181836. 그림 확대/그림 확대.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String[] picture, int k) {
+        
         String[] answer = new String[picture.length * k];
         
         int answerIdx = 0;
@@ -17,13 +18,14 @@ class Solution {
                 for(int j = 0; j < k; j++) {
                     temp += pixel;
                 }
-                
-            }
+            }//행 문자열 생성 끝
             
             //세로 문자열 생성
-            for(int i = 0; i < k; i++) answer[answerIdx++] = temp;
+            for(int i = 0; i < k; i++) {
+                answer[answerIdx++] = temp;
+            }//세로 문자열 생성 끝
             
-        }
+        }//바깥 for문 끝
         
         return answer;
     }
